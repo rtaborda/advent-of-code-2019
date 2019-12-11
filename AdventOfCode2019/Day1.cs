8b0680b898727df6b62b2590dad158fb347a1191
Rui Taborda <rtaborda@obi4wan.com>
2019-12-11 12:42:59 +0100

    Solutions for Part 1 and 2 of Day 1

---
+++ AdventOfCode2019/Day1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public class Day1
+    {
+        private readonly List<double> _modulesMass;
+
+        public Day1()
+        {
+            var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Inputs", "Day1.txt"));
+            _modulesMass = new List<double>(lines.Length);
+            _modulesMass.AddRange(lines.Select(double.Parse));
+        }
+
+        public int ExecutePart1() => _modulesMass.Sum(CalculateMassFuel);
+
+        public int ExecutePart2() => _modulesMass.Sum(CalculateFuelForFuelMass);
+
+        private static int CalculateMassFuel(double mass) => (int)(Math.Floor(mass / 3) - 2);
+
+        private static int CalculateFuelForFuelMass(double mass)
+        {
+            var moduleFuel = CalculateMassFuel(mass);
+            var fuelMassFuel = moduleFuel;
+            while (fuelMassFuel > 0)
+            {
+                fuelMassFuel = CalculateMassFuel(fuelMassFuel);
+                if (fuelMassFuel > 0)
+                {
+                    moduleFuel += fuelMassFuel;
+                }
+            }
+
+            return moduleFuel;
+        }
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var day1 = new Day1();
+            Console.WriteLine($"Day 1 Part 1: {day1.ExecutePart1()}");
+            Console.WriteLine($"Day 1 Part 1: {day1.ExecutePart2()}");
+
+            Console.ReadKey();
+        }
+    }
+}
